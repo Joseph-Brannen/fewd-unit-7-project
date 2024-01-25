ea3ae9736b2f30305500108c3be1dc863589ad69
Joseph-Brannen <141746228+Joseph-Brannen@users.noreply.github.com>
2024-01-25 10:19:17 -0500

    Add bell transition and removes icon when alert is cleared

--- app.js
+++ app.js
@@ -3,8 +3,10 @@ const alert = document.getElementById('alert');
 alert.addEventListener( 'click', (e) => {
     const buttonAlert = document.querySelector('#alert button');
     const paragraphAlert = document.querySelector('#alert p');
+    const notification = document.querySelector('#notification');
     if (buttonAlert === e.target || paragraphAlert === e.target) {
         alert.remove();
+        notification.style.opacity = '0';
     }
 });
 
