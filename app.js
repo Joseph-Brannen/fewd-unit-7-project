3e996231b8032d054abdb8692df21faade3020b9
Joseph-Brannen <141746228+Joseph-Brannen@users.noreply.github.com>
2024-02-08 17:01:37 -0500

    Add partial completion of step 6 (html, css, js)

--- app.js
+++ app.js
@@ -10,3 +10,62 @@ alert.addEventListener( 'click', (e) => {
     }
 });
 
+
+
+
+
+
+const messageUser = document.querySelector('.message-user');
+
+messageUser.addEventListener( 'click', (e) => {
+    const sendMessage = document.getElementById('send-message');
+    const user = document.querySelector('.messaging input');
+    const textarea = document.querySelector('textarea');
+    const error = document.getElementById('error');
+    const confirm = document.getElementById('confirm');
+    user.text = '';
+    textarea.text = '';
+    if ( sendMessage === e.target && (user.value === '' || textarea.value === '') ) {
+        console.log('wrong');
+        error.style.display = "block";
+        sendMessage.style.display = "none";
+        user.style.display = "none";
+        textarea.style.display = "none";
+    } else if ( sendMessage === e.target && user.value !== '' && textarea.value !== '') {
+        console.log('right');
+        confirm.style.display = "block";
+        sendMessage.style.display = "none";
+        user.style.display = "none";
+        textarea.style.display = "none";
+    }
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
